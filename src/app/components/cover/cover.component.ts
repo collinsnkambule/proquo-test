7aecae368b232e42e170a67a8640f40cd73ae011
Collins Nkambule <sketo.nkambule@eoh.com>
2023-04-25 00:08:20 +0200

    Styling

--- src/app/components/cover/cover.component.ts
+++ src/app/components/cover/cover.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
   selector: 'app-cover',
@@ -8,4 +9,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './cover.component.html',
   styleUrls: ['./cover.component.scss'],
 })
-export class CoverComponent {}
+export class CoverComponent implements OnInit{
+  /**
+   *
+   */
+  menus:unknown = [];
+  constructor(private backendService: BackendService) {
+    
+  }
+  ngOnInit(): void {
+    this.backendService.fetchMenuItems().then(items =>{
+      this.menus = items
+
+    })
+  }
+}
